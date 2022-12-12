679fac405aed482aea3813a5a2df8d66d48d940b
oui <triggerlesgolems00@gmail.com>
2022-12-12 19:44:30 +0100

    save

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const pagesInput = document.getElementById("pages");
 const readInput = document.getElementById("read");
 const card = document.getElementsByClassName("card");
 const addButton = document.getElementById("add-button");
+const modal = document.getElementById("myModal");
+const showModal = document.getElementById("add-button");
+const closeSpan = document.getElementsByClassName("close")[0];
 
 let myLibrary = [];
 
@@ -89,26 +92,16 @@ function resetFields() {
         inputArray[3].checked = false;
     });
 }
-// Get the modal
-const modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-const btn = document.getElementById("add-button");
-
-// Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
+showModal.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+
+closeSpan.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
